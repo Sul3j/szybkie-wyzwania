5c99b5194366f68a55ee68236ce5624d0fd32d0e
Szymon Sulejczak <contact@szymonsulejczak.com>
2025-12-25 16:28:25 +0100

    add accounts serializers

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,130 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from .models import UserProfile
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.EmailField(source='user.email', read_only=True)
+    solved_count = serializers.IntegerField(read_only=True)
+    total_submissions = serializers.IntegerField(read_only=True)
+    acceptance_rate = serializers.FloatField(read_only=True)
+    easy_solved = serializers.SerializerMethodField()
+    medium_solved = serializers.SerializerMethodField()
+    hard_solved = serializers.SerializerMethodField()
+
+    class Meta:
+        model = UserProfile
+        fields = [
+            'id', 'username', 'email', 'bio', 'avatar',
+            'experience_points', 'level', 'rank',
+            'solved_count', 'total_submissions', 'acceptance_rate',
+            'easy_solved', 'medium_solved', 'hard_solved',
+            'created_at', 'updated_at'
+        ]
+        read_only_fields = ['experience_points', 'level', 'rank', 'created_at', 'updated_at']
+
+    def get_easy_solved(self, obj):
+        from apps.problems.models import Problem
+        solved_problem_ids = obj.user.submission_set.filter(
+            status='accepted'
+        ).values_list('problem_id', flat=True).distinct()
+
+        return Problem.objects.filter(
+            id__in=solved_problem_ids,
+            difficulty='easy'
+        ).count()
+    
+    def get_medium_soved(self, obj):
+        from apps.problems.models import Problem
+        solved_problem_ids = obj.user.submission_set.filter(
+            status='accepted'
+        ).values_list('problem_id', flat=True).distinct()
+
+        return Problem.objects.filter(
+            id__in=solved_problem_ids,
+            difficulty='medium'
+        ).count()
+    
+    def get_hard_solved(self, obj):
+        from apps.problems.models import Problem
+        solved_problem_ids = obj.user.submission_set.filter(
+            status='accepted'
+        ).values_list('problem_id', flat=True).distinct()
+
+        return Problem.objects.filter(
+            id__in=solved_problem_ids,
+            difficulty='hard'
+        ).count()
+    
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'profile']
+        read_only_fields = ['id']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    password_confirm = serializers.CharField(write_only=True, required=True)
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_confirm']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({
+                "password": "Password fields didn't match."
+            })
+        return attrs
+    
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already in use.")
+        return value
+    
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+    
+class ChangePasswordSerializer(serializers.Serializer):
+    
+    old_password = serializers.CharField(required=True, write_only=True)
+    new_password = serializers.CharField(
+        required=True,
+        write_only=True,
+        validators=[validate_password]
+    )
+    new_password_confirm = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['new_password_confirm']:
+            raise serializers.ValidationError({
+                "new_password": "Password fields didn't match."
+            })
+        return attrs
+    
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("Old password is incorrect.")
+        return value
+    
+    def save(self, **kwargs):
+        user = self.context['request'].user
+        user.set_password(self.validated_data['new_password'])
+        user.save()
+        return user
