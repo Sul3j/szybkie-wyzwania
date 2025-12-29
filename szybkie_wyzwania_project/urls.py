db7eb1581659dbeec77fe584a7a91d6fb514e71b
Szymon Sulejczak <contact@szymonsulejczak.com>
2025-12-29 18:31:46 +0100

    add main app urls

--- szybkie_wyzwania_project/urls.py
+++ szybkie_wyzwania_project/urls.py
@@ -1,22 +1,36 @@
-"""
-URL configuration for szybkie_wyzwania_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import (
+    home_view,
+    problems_view,
+    problem_detail_view,
+    leaderboard_view,
+    profile_view,
+    register_view,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/auth/', include('apps.accounts.urls')),
+    path('api/problems/', include('apps.problems.urls')),
+    path('api/submissions/', include('apps.submissions.urls')),
+    path('api/leaderboard/', include('apps.leaderboard.urls')),
+
+    path('', home_view, name='home'),
+    path('register/', register_view, name='register'),
+    path('problems/', problems_view, name='problems'),
+    path('problems/<slug:slug>/', problem_detail_view, name='problem-detail'),
+    path('leaderboard/', leaderboard_view, name='leaderboard'),
+    path('profile/', profile_view, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Szybkie Wyzwania Admin"
+admin.site.site_title = "Szybkie Wyzwania"
+admin.site.index_title = "Welcome to Szybkie Wyzwania Administration"
